f8b8a1ea4844e75f6bd769474d300472f6b0127f
josemiguelq <jose.miguelq95@gmail.com>
2019-10-04 07:49:35 -0300

    booking de spot

---
+++ src/controllers/BookingController.js
@@ -0,0 +1,22 @@
+const Spot = require('./../models/Spot')
+const Booking = require('./../models/Booking')
+module.exports = {
+    async store(req, res) {
+        const {filename} = req.file
+        const {company, techs, price} = req.body
+        const {user_id} = req.headers
+        spot = await Spot.create({
+            user: user_id,
+            company,
+            price,
+            thumbnail: filename,
+            techs: techs.split(',').map(tech => tech.trim())
+        })
+        return res.json(spot)
+    },
+    async index(req, res) {
+        const { tech } =  req.query
+        const spots = await Spot.find({techs: tech})
+        return res.json(spots)
+    }
+}
--- src/routes/spot.js
+++ src/routes/spot.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const multer = require('multer')
 const SpotController = require('./../controllers/SpotController')
+const BookingController = require('./../controllers/BookingController')
 const UploadConfig = require('./../config/upload')
 const upload = multer(UploadConfig)
 
@@ -8,5 +9,6 @@ const routes = express.Router()
 
 routes.post('/spot', upload.single('thumbnail'), SpotController.store);
 routes.get('/spot', SpotController.index);
+routes.post('/spot/spot_id/booking', BookingController.store);
 
 module.exports = routes;
